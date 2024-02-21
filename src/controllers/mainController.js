3e186ee970d6d939e5123d578335ff53c28193f4
Fabien Thiesset <thiessetfabien3@gmail.com>
2024-02-21 10:35:37 +0100

    feet/view home page

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,8 +1,18 @@
-const mainController = {
-  renderHomePage(req, res) {
-    res.render("home");
-  }
-};
+const { Quiz } = require('../models');
 
+
+const mainController = {
+  async renderHomePage(req, res) {
+    try {
+      const Quizs = await Quiz.findAll({ include: [ "author",  "tags" ]});
+      
+      res.render("home", { Quizs });
+
+    } catch (error) {
+      console.error(error);
+      res.status(500).render("500");
+    }
+  },
+};
 
 module.exports = mainController;
